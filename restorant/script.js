a484880f3c4446a39d457b46ca90c794344f45e4
gilbrtdusingize <gilbrtdusingize@gmail.com>
2026-02-21 10:57:15 +0200

    Cleanup: Remove redundant subfolders

--- restorant/script.js
+++
@@ -1,5 +0,0 @@
-const headerMenuToggle=document.querySelector(".header__menu-toggle");
-const headerMenus=document.querySelector(".header-menus");
-headerMenuToggle.addEventListener('click',()=>{
-    headerMenus.classList.toggle('active');
-});
